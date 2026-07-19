e93cab67bfae73dbdc02b4a395b14ddc223151e1
edchalk96 <edchalk96@gmail.com>
2026-07-19 22:43:00 +0100

    Added in code and styling for navbar video overlay

--- static/js/base.js
+++ static/js/base.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+    // Hero Video Play/Pause Toggle
     const video = document.getElementById('hero-video');
     const button = document.getElementById('video-toggle-btn');
     const icon = document.getElementById('toggle-icon');
@@ -16,4 +18,19 @@ document.addEventListener('DOMContentLoaded', function() {
             button.setAttribute('aria-label', 'Play Video');
         }
     });
+
+    // Sticky Navigation Bar on Scroll | Credit to https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollY
+    const navbar = document.querySelector('.homepage-sticky-nav');
+
+    if (navbar && video) {
+        window.addEventListener('scroll', function() {
+            const scrollTriggerPoint = video.offsetHeight / 2;
+
+            if (window.scrollY > scrollTriggerPoint) {
+                navbar.classList.add('scrolled');
+            } else {
+                navbar.classList.remove('scrolled');
+            }
+        });
+    }
 });
