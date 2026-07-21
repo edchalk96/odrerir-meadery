ffad13bc706a297604b819b754a673c5d89a243e
edchalk96 <edchalk96@gmail.com>
2026-07-21 15:18:48 +0100

    Added styling and animation to products menu

--- static/js/base.js
+++ static/js/base.js
@@ -20,22 +20,23 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     // Sticky Navigation Bar on Scroll | Credit to https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollY
-    const navbar = document.querySelector('.homepage-sticky-nav');
+    const navbarSticky = document.querySelector('.homepage-sticky-nav');
 
-    if (navbar && video) {
+    if (navbarSticky && video) {
         window.addEventListener('scroll', function() {
             const scrollTriggerPoint = video.offsetHeight / 2;
 
             if (window.scrollY > scrollTriggerPoint) {
-                navbar.classList.add('scrolled');
+                navbarSticky.classList.add('scrolled');
             } else {
-                navbar.classList.remove('scrolled');
+                navbarSticky.classList.remove('scrolled');
             }
         });
     }
 
     // Remove focus from toggler when collapsed
     const toggler = document.querySelector('.navbar-toggler');
+
     if (toggler) {
         toggler.addEventListener('click', function() {
             setTimeout(function() {
@@ -44,13 +45,55 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    // Removve focus from dropdowns when closed
-    const dropdownButtons = document.querySelectorAll('.dropdown-toggle');
-    dropdownButtons.forEach(button => {
-        button.addEventListener('hidden.bs.dropdown', function() {
-            this.blur();
+    // Navbar products menu transition
+    const navbar = document.querySelector('.navbar-bg');
+    const productsToggle = document.getElementById('navbarDropdownProducts');
+    const dropdownMenu = productsToggle.nextElementSibling;
+
+    if (productsToggle && navbar) {
+
+        // Opening
+        productsToggle.addEventListener('show.bs.dropdown', function() {
+            navbar.classList.add('navbar-products-open');
+
+            if (dropdownMenu) {
+                dropdownMenu.style.display = 'block';
+
+                requestAnimationFrame(() => { /* https://www.w3schools.com/jsref/met_win_requestanimationframe.asp */
+                    requestAnimationFrame(() => {
+                        dropdownMenu.classList.add('show')
+                    })
+                })
+            }
         });
-    });
+
+        // Closing
+        productsToggle.addEventListener('hide.bs.dropdown', function(e) {
+            productsToggle.blur();
+            navbar.classList.remove('navbar-products-open');
+
+            if (dropdownMenu && !dropdownMenu.classList.contains('is-collapsing')) {
+                e.preventDefault() // https://getbootstrap.com/docs/4.0/getting-started/javascript/#:~:text=All%20infinitive%20events%20provide%20preventDefault,also%20automatically%20call%20preventDefault()%20
+                
+                dropdownMenu.classList.remove('show');
+                productsToggle.classList.remove('show');
+                productsToggle.setAttribute('aria-expanded', 'false');
+
+                dropdownMenu.classList.add('is-collapsing');
+
+                setTimeout(() => {
+                    dropdownMenu.classList.remove('is-collapsing');
+                    dropdownMenu.style.display = '';
+
+                    const bsDropdown = bootstrap.Dropdown.getInstance(productsToggle);
+                    if (bsDropdown) {
+                        dropdownMenu.classList.remove('show');
+                        productsToggle.classList.remove('show');
+                    }
+                }, 600);
+            }
+        });
+    }
 
     // Close Django messages after set period of time
     setTimeout(function() {
