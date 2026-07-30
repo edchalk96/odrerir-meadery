f4f6f221ebd53247535a5b91ee884d9b029bdaa7
edchalk96 <edchalk96@gmail.com>
2026-07-30 21:20:29 +0100

    added styling to basket and js bug fix

--- basket/static/basket/js/basket.js
+++ basket/static/basket/js/basket.js
@@ -1,53 +1,50 @@
-function handleEnableDisable(itemId, volume) {
-    var inputElement = $(`.id_qty_${itemId}`);
+function handleEnableDisable(element) {
+    var container = $(element).closest('.update-form');
+    var inputElement = container.find('.qty_input');
     var currentValue = parseInt(inputElement.val()) || 1;
+
     var minusDisabled = currentValue < 2;
     var plusDisabled = currentValue > 98;
-    $(`.decrement-qty_${itemId}`).prop('disabled', minusDisabled);
-    $(`.increment-qty_${itemId}`).prop('disabled', plusDisabled);
+
+    container.find('.decrement-qty').prop('disabled', minusDisabled);
+    container.find('.increment-qty').prop('disabled', plusDisabled);
 }
 
-var allQtyInputs = $('.qty_input');
-for (var i = 0; i < allQtyInputs.length; i++) {
-    var itemId = $(allQtyInputs[i]).data('item_id');
-    var currentVolume = $('input[name="product_volume"]:checked').val();
-    handleEnableDisable(itemId, currentVolume);
-}
+$('.qty_input').each(function() {
+    handleEnableDisable(this);
+});
 
 $('.qty_input').on('change input', function() {
-    var itemId = $(this).data('item_id');
-    var currentVolume = $('input[name="product_volume"]:checked').val();
     var val = parseInt($(this).val());
+
     if (isNaN(val) || val < 1) {
         $(this).val(1);
     } else if (val > 99) {
         $(this).val(99);
     }
-    handleEnableDisable(itemId, currentVolume);
+    handleEnableDisable(this);
 });
 
 $('.increment-qty').click(function(e) {
     e.preventDefault();
-    var itemId = $(this).data('item_id');
-    var volume = $('input[name="product_volume"]:checked').val();
+
     var inputField = $(this).closest('.input-group').find('.qty_input');
     var currentValue = parseInt(inputField.val()) || 1;
     if (currentValue < 99) {
         inputField.val(currentValue + 1);
     }
-    handleEnableDisable(itemId, volume);
+    handleEnableDisable(this);
 });
 
 $('.decrement-qty').click(function(e) {
     e.preventDefault();
-    var itemId = $(this).data('item_id');
-    var volume = $('input[name="product_volume"]:checked').val();
+
     var inputField = $(this).closest('.input-group').find('.qty_input');
     var currentValue = parseInt(inputField.val()) || 1;
     if (currentValue > 1) {
         inputField.val(currentValue - 1);
     }
-    handleEnableDisable(itemId, volume);
+    handleEnableDisable(this);
 });
 
 $('.update-link').click(function(e) {
